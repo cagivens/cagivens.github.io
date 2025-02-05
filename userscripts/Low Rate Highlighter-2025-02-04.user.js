5c9bd83eeeb589e7aff37152d16286f3afe8121b
Caleb Givens <33668778+cagivens@users.noreply.github.com>
2025-02-05 16:31:26 -0500

    Update Low Rate Highlighter-2025-02-04.user.js

--- userscripts/Low Rate Highlighter-2025-02-04.user.js
+++ userscripts/Low Rate Highlighter-2025-02-04.user.js
@@ -31,11 +31,25 @@
         const targetRate = 26;
         // Getting rows that contain UPH
         const rows = $('#secondaryProductivityList tr').toArray();
+        const headerElements = $('#secondaryProductivityList th').toArray();
+        var uphIndex = 0;
+        // Finding the correct column index to highlight
+        for(var h = 0; h < $('#secondaryProductivityList th').toArray().length; h++) {
+            try {
+                const label = headerElements[h].getElementsByTagName('div')[0].innerHTML;
+                if(label.includes("UPH")) {
+                    uphIndex = h - 2;
+                    break;
+                }
+            } catch(e) {
+                console.error(e.name + ' has occurred\n' + e.message);
+            }
+        }
         // Giving each row a new class for later animation
         for(var i = 2; i < rows.length; i++) {
             try {
                 const fieldContainers = rows[i].getElementsByTagName('td');
-                const rateContainer = fieldContainers[10];
+                const rateContainer = fieldContainers[uphIndex];
                 const currentRate = Number(rateContainer.innerHTML);
             if(currentRate < targetRate) {
                 rateContainer.classList.add('lowRate');
