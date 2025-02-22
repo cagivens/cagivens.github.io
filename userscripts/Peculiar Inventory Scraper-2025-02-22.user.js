2b7e7b12b6a13186ee694b5091afd247a544f447
Caleb Givens <33668778+cagivens@users.noreply.github.com>
2025-02-22 13:37:21 -0500

    Add files via upload

---
+++ userscripts/Peculiar Inventory Scraper-2025-02-22.user.js
@@ -0,0 +1,76 @@
+// ==UserScript==
+// @name         Peculiar Inventory Scraper
+// @namespace    https://cagivens.github.io/
+// @version      2025-02-22
+// @description  Searches currently viewed peculiar inventory bucket for records, then downloads them as .csv
+// @author       caleigiv
+// @match        http://peculiar-inventory-na.aka.corp.amazon.com/MDT1/report/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $(document).ready(function(){
+        $('.a-input-text-wrapper.a-form-selected.a-width-extra-large').append('<button class="listifyButton">Press Me!</button>');
+        $('.listifyButton').click(function(){
+            function saveCSV(data) {
+                const url = window.URL;
+                var filename = 'peculiar_inventory_data.csv';
+                var file = new Blob([data], {type: 'text/plain'});
+                var downloadElement = document.createElement('a');
+                downloadElement.setAttribute('href', window.URL.createObjectURL(file));
+                downloadElement.setAttribute('download', filename);
+                document.body.appendChild(downloadElement);
+                downloadElement.click();
+                document.removeChild(downloadElement);
+                debugger;
+            }
+            function sweepContainer(containerArray) {
+                var resultString = '';
+                for(var i = 0; i < containerArray.length; i++) {
+                    containerArray[i].getElementsByTagName('button')[0].click();
+                    const currentContainer = containerArray[i].getElementsByClassName('textCol')[1].firstChild == null ? '' : containerArray[i].getElementsByClassName('textCol')[1].firstChild.innerHTML;
+                    const containerType = containerArray[i].getElementsByClassName('textCol')[2].innerHTML;
+                    const parent = containerArray[i].getElementsByClassName('textCol')[3].firstChild == null ? '' : containerArray[i].getElementsByClassName('textCol')[3].firstChild.innerHTML;
+                    const parentType = containerArray[i].getElementsByClassName('textCol')[4].innerHTML;
+                    const grandparent = containerArray[i].getElementsByClassName('textCol')[5].firstChild == null ? '' : containerArray[i].getElementsByClassName('textCol')[5].firstChild.innerHTML;
+                    const grandparentType = containerArray[i].getElementsByClassName('textCol')[6].innerHTML;
+                    const totalRecords = Number(document.getElementById('container-inventory-details_info').innerHTML.split(' ')[5]);
+                    var scrapedRecords = 0;
+                    var items = document.getElementById('side-panel').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+                    var nextButton = document.getElementById('container-inventory-details_next');
+                    for(var j = 0; j < items.length; j++) {
+                        const data = items[j].getElementsByTagName('td')
+                        resultString += currentContainer + ',';
+                        resultString += containerType + ',';
+                        resultString += parent + ',';
+                        resultString += parentType + ',';
+                        resultString += grandparent + ',';
+                        resultString += grandparentType + ',';
+                        resultString += data[0].innerHTML + ',';
+                        resultString += data[1].getElementsByTagName('a')[0].innerHTML + ',';
+                        resultString += data[2].innerHTML + ',';
+                        resultString += data[3].innerHTML + ',';
+                        resultString += data[4].innerHTML + ',\n';
+                        scrapedRecords += 1;
+                        if(j == items.length - 1) {
+                            debugger;
+                            if(scrapedRecords < totalRecords - 1) {
+                                nextButton.click();
+                                j = 0;
+                                items = document.getElementById('side-panel').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+                                debugger;
+                            }
+                        }
+                    }
+                }
+                return resultString;
+            }
+            var bucketData = 'Container,Container Type,Parent,Parent Type,Grandparent,Grandparent Type,Estimated Age,FcSku,Quantity,Owner,Disposition,\n';
+            bucketData += sweepContainer($('tbody').toArray()[1].getElementsByTagName('tr'));
+            saveCSV(bucketData);
+        });
+    });
+})();
